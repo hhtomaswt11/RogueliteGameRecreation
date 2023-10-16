150d0ac45c17f54cb599723e786cc7da240e4495
Tomás Melo <115300193+codingtomasZ@users.noreply.github.com>
2023-10-16 20:04:04 +0100

    Update utils.c

--- src/engine/utils/utils.c
+++ src/engine/utils/utils.c
@@ -1,13 +1,7 @@
 #include "utils.h"
 #include <stdlib.h>
 
-/*
 
-* a104356 - João Lobo
-
-* Retorna o número de caracteres newline numa string.
-
-*/
 int count_newlines(char text[]){
     int count = 0;
     for(int i = 0; text[i] != '\0'; i++){
