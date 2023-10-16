065484473ccc6f1adecfaff64988479532554eeb
Tomás Melo <115300193+codingtomasZ@users.noreply.github.com>
2023-10-16 20:12:15 +0100

    Update beacon.c

--- src/game/player/beacon.c
+++ src/game/player/beacon.c
@@ -4,26 +4,17 @@
 #include <stdlib.h>
 #include "draw.h"
 
-/*
+// Coloca um beacon de luz no mapa.
 
-* a104356 - João Lobo
 
-* Coloca um beacon de luz no mapa.
-
-*/
 void place_beacon(Vector2D pos, World *world){
     world->beaconLocations = realloc(world->beaconLocations, sizeof(Vector2D) * (world->beacons + 1));
     world->beaconLocations[world->beacons] = pos;
     world->beacons++;
 }
 
-/*
+// Desenha um beacon de luz.
 
-* a104356 - João Lobo
-
-* Desenha um beacon de luz.
-
-*/
 void draw_beacon(Vector2D pos, Terminal *terminal){
     draw_custom_pixel(pos, "||", 59, 96, terminal);
 }
